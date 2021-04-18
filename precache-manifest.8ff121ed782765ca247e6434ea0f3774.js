78a82b9691fe69b04eac90b2786d07585df5f723
YajithVishwa <yajithvishwa2001@gmail.com>
2021-04-18 19:35:06 +0530

    Updates

---
+++ precache-manifest.8ff121ed782765ca247e6434ea0f3774.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1f5a0cce4ec9a40101ddef1bd3e838",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b43b23359e80121aea0e",
+    "url": "/home/static/css/main.d11511b8.chunk.css"
+  },
+  {
+    "revision": "4f5dcedf1822c5d06d61",
+    "url": "/home/static/js/2.ea6bb60a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.ea6bb60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43b23359e80121aea0e",
+    "url": "/home/static/js/main.e165a2bd.chunk.js"
+  },
+  {
+    "revision": "1c243cb515b399fd2de9",
+    "url": "/home/static/js/runtime-main.02298bcf.js"
+  },
+  {
+    "revision": "1739cb55b675fac3f652276c438155e2",
+    "url": "/home/static/media/image.1739cb55.png"
+  },
+  {
+    "revision": "37660bdb33092d8f2d7c879d975fd7bd",
+    "url": "/home/static/media/jakstech.37660bdb.png"
+  },
+  {
+    "revision": "fade4759c07f25a0da3e88d479751766",
+    "url": "/home/static/media/resume.fade4759.pdf"
+  }
+]);
